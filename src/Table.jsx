8354c13f8743102cf033b7a9e025507aa5650d0f
Musaib Altaf <85894666+musaib-js@users.noreply.github.com>
2023-08-04 13:05:04 +0530

    Update Table.jsx to make it resizable

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import { Resizable } from 'react-resizable';
 
-const Table = ({ data }) => {
+const ResizableCell = ({ children, width, ...rest }) => {
+  return (
+    <Resizable width={width} height={0} handle={<div className="react-resizable-handle" />} {...rest}>
+      <div>{children}</div>
+    </Resizable>
+  );
+};
+
+const Table = ({ data, width }) => {
   if (!data || data.length === 0) {
     return <p>No data to display.</p>;
   }
@@ -8,24 +17,30 @@ const Table = ({ data }) => {
   const headers = Object.keys(data[0]);
 
   return (
-    <table className="table table-striped table-responsive">
-      <thead>
-        <tr>
-          {headers.map((header, index) => (
-            <th key={index}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item, rowIndex) => (
-          <tr key={rowIndex}>
-            {headers.map((header, colIndex) => (
-              <td key={colIndex}>{item[header]}</td>
+    <div style={{ width: width || '100%' }}>
+      <table className="table table-striped table-responsive">
+        <thead>
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index} className="resizable-header">
+                <ResizableCell width={100}>
+                  <div>{header}</div>
+                </ResizableCell>
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((item, rowIndex) => (
+            <tr key={rowIndex}>
+              {headers.map((header, colIndex) => (
+                <td key={colIndex}>{item[header]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
